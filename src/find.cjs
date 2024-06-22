902bf6f60b8681a9088228ed1683939bd5ec3d62
BaloZ <81485635+b4l0z@users.noreply.github.com>
2024-06-22 13:21:44 +0200

    Update find.cjs

--- src/find.cjs
+++ src/find.cjs
@@ -1,30 +1,31 @@
 const network = require("network");
 const fs = require("node:fs");
 const ip = require("./ip.json");
+const { red, green, magenta } = require("console-log-colors");
 
 network.get_private_ip(function (err, localIP) {
   if (err) {
-    console.error("Une erreur est survenue lors de la récupération de l'IP locale.", err);
+    console.log(red("Une erreur est survenue lors de la récupération de l'IP locale.", err));
     return;
   }
 
-  console.log(`IP Locale: ${localIP}`);
+  console.log(green(`IP Locale:`), magenta(`${localIP}`));
 
   const parts2 = localIP.split(".");
   if (parts2.length >= 4) {
     const thirdOctet2 = parts2[2];
-    console.log(`Troisième octet: ${thirdOctet2}`);
+    console.log(green(`Troisième octet:`), magenta(` ${thirdOctet2}`));
     ip.third_octet = parseInt(thirdOctet2);
     ip.localIP = localIP;
     saveIP();
   } else {
-    console.log("Adresse IP Invalide");
+    console.log(red("Adresse IP Invalide"));
   }
 });
 
 function saveIP() {
   fs.writeFile(`./src/ip.json`, JSON.stringify(ip, null, 4), (err) => {
-    console.log("Enresgitré dans ip.json");
+    console.log(green("Enregistré dans"), magenta(" ip.json"));
     if (err) console.log(err);
   });
 }
