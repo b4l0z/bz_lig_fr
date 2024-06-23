b3e91a749928932720eaa659992e03c4c99d847f
BaloZ <81485635+b4l0z@users.noreply.github.com>
2024-06-23 12:20:50 +0200

    Update ping.cjs

--- src/ping.cjs
+++ src/ping.cjs
@@ -2,28 +2,33 @@ const os = require("os");
 const dns = require("dns");
 const ping = require("ping");
 const { red, green, magenta } = require("console-log-colors");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const path = require("path");
+const { Server } = require("socket.io");
+const io = new Server(server);
+const ipfile = require("./ip.json");
+const network = require("network");
+const fs = require("fs");
 
-console.log("");
-console.log("");
+let currentPublicIp = ipfile.currentPublicIP;
 
-//////////////////////////////////////////
+app.use("/font", express.static(path.join(__dirname, "font")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../app.html"));
+});
+
+server.listen(3000, () => {
+  console.log(`Server is running at http://localhost:3000`);
+});
 
 const subnet = `192.168.${require("./ip.json").third_octet}.`;
 const start = 1;
 const end = 254;
 
-function getLocalIP() {
-  const ifaces = os.networkInterfaces();
-  let localIP = null;
-  Object.keys(ifaces).forEach((ifname) => {
-    ifaces[ifname].forEach((iface) => {
-      if (iface.family === "IPv4" && !iface.internal) {
-        localIP = iface.address;
-      }
-    });
-  });
-  return localIP;
-}
 async function getHostname(ip) {
   return new Promise((resolve, reject) => {
     dns.reverse(ip, (err, hostnames) => {
@@ -35,7 +40,6 @@ async function getHostname(ip) {
     });
   });
 }
-
 async function scanIPRange() {
   const promises = [];
   const foundIPs = [];
@@ -49,16 +53,45 @@ async function scanIPRange() {
           if (ip == require("./ip.json").localIP) return;
           if (hostname) {
             console.log(green(`${ip}`), magenta(`(${hostname})`));
+            ipfile[currentPublicIp][ip] = {};
+            ipfile[currentPublicIp][ip]["ip"] = ip;
+            ipfile[currentPublicIp][ip]["alive"] = true;
+            ipfile[currentPublicIp][ip]["ping"] = res.avg;
+            ipfile[currentPublicIp][ip]["hostName"] = hostname;
+            saveIP();
           } else {
             console.log(green(`${ip}`));
+            ipfile[currentPublicIp][ip] = {};
+            ipfile[currentPublicIp][ip]["ip"] = ip;
+            ipfile[currentPublicIp][ip]["alive"] = true;
+            ipfile[currentPublicIp][ip]["ping"] = res.avg;
+            saveIP();
           }
           foundIPs.push(ip);
         }
       })
     );
   }
+
   await Promise.all(promises);
   return foundIPs;
 }
-
 scanIPRange();
+
+io.on("connection", (socket) => {
+  for (let ipAddress in ipfile[currentPublicIp]) {
+    const item = ipfile[currentPublicIp][ipAddress];
+    console.log(item); // Affichez l'item dans la console
+
+    // Émettez chaque item via Socket.IO
+    io.emit("ip_data", item);
+    io.emit("local_ip", ipfile.localIP);
+  }
+});
+
+function saveIP() {
+  fs.writeFile(`./src/ip.json`, JSON.stringify(ipfile, null, 4), (err) => {
+    console.log(green("Enregistré dans"), magenta(" ip.json"));
+    if (err) console.log(err);
+  });
+}
